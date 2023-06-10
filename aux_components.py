8a470391db67d8145e188a35ce227ad6968e9b11
BadiRodriguez <badirodriguez.csci@gmail.com>
2023-06-10 12:54:28 -0500

    Update aux_components.py

--- aux_components.py
+++ aux_components.py
@@ -1,10 +1,11 @@
-import random
-
-def gen_board(x: int, y: int):
-    """Generates board of dimensions "x" x "y", display in strings for simple assimilation"""
+def generate_board(x: int, y: int):
+    """Generates board of dimensions "x" x "y", display in strings for simple assimilation."""
     board = []
+    total_nums = x * y
+
+    visual_board = ''
     for i in range(0, y):
-        row = f'[.]' * x
+        row = f'[ ]' * x
         board.append(row)
     return board
 
@@ -12,9 +13,6 @@ def display_board(board: list):
     """Displays the board taking into account dimensional spaces."""
     for row in board:
         print(' '.join(row))
-
-def fill_board(board: list):
-    total_nums = x * y
 
 
 
