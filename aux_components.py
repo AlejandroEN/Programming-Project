c2e2356e8f62065585ac86537079432e316d43a7
BadiRodriguez <badirodriguez.csci@gmail.com>
2023-06-10 13:42:42 -0500

    Update aux_components.py

--- aux_components.py
+++ aux_components.py
@@ -1,5 +1,6 @@
 def generate_board(x: int, y: int):
-    """Generates board of dimensions "x" x "y", display in strings for simple assimilation."""
+    """Generates board of dimensions "x" x "y", display in strings for simple assimilation.
+    'x' represents amount of columns, and 'y' represents amount of rows"""
     board = []
     total_nums = x * y
 
@@ -13,6 +14,31 @@ def display_board(board: list):
     """Displays the board taking into account dimensional spaces."""
     for row in board:
         print(' '.join(row))
+
+def initialize_game(difficulty:int, number_of_players:int, board:list):
+    """Begins sequence for the game after selecting difficulty, number of players, and instance.
+        For difficulty: 0 = Easy, 1 = Normal, 2 = Hard."""
+    if difficulty == 0:
+        return generate_board(4, 4)
+    elif difficulty == 1:
+        return generate_board(8, 4)
+    elif difficulty == 2:
+        return generate_board(13, 4)
+    else:
+        return print('Invalid difficult selected.')
+
+
+B = generate_board(3, 2)
+display_board(B)
+
+status = (initialize_game(2, 0, 0))
+
+B = status
+display_board(B)
+
+
+
+
 
 
 
