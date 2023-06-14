45f6689fe777a48bdcf6f2a7326282c7060d0c53
AlejandroEN <alejandroescobarnunez@gmail.com>
2023-06-14 04:46:38 -0500

    v0.9
    - Minor changes in format.

--- board.py
+++ board.py
@@ -42,19 +42,10 @@ def fill_board(board: list, difficulty: int):
         case _: print('System error.')
 
     card_pair = card.get_cards(n)
+
     for row in board:
         for column in range(0, len(row)):
             row[column] = f"[{card_pair[step]}]"
             step += 1
-    step +=4
 
-
-board = generate_board(4, 8)
-display_board(board)
-print(board[1])
-print(board[2][1])
-print(board[0])
-print(board[3])
-print("- - - - - - - -")
-fill_board(board, 1)
-display_board(board)
+    step += 4
--- user.py
+++ user.py
@@ -1,4 +1,5 @@
 import json
+
 def create_new_user(username: str, password: str):
     new_player = {"username": username, "password": password}
     json_object = json.dumps(new_player, indent=4)
