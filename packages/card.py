5e17b8709c868a4311a53827c1708fac63c217de
BadiRodriguez <badirodriguez.csci@gmail.com>
2023-07-02 17:10:50 -0500

    v.1.9+
    Minor trims and fixes:
    
    +Addition of descriptions for each function on card.py module
    
    - Syntax polishments

--- packages/card.py
+++ packages/card.py
@@ -9,6 +9,12 @@ _EXTENDED_VALUE: dict[str, str] = {value: key for key, value in _SHORT_VALUE.ite
 _NUMBER_VALUE: dict[str, int] = {"J": 11, "Q": 12, "K": 13, "A": 14}
 
 def get_pairs_of_cards(number_of_different_cards: int) -> list[Card]:
+    """
+    Function generates a list that contains a pair of cards, utilizing the class Card.
+    Defines a set number of cards to generate, appends them on list and shuffles said list.
+    Each card is assigned both their respective values and hidden values.
+    """
+
     hand: Stack = _get_hand(number_of_different_cards)
     list_of_cards: list[Card] = []
 
@@ -23,6 +29,10 @@ def get_pairs_of_cards(number_of_different_cards: int) -> list[Card]:
     return list_of_cards
 
 def get_players_order(players: list[Player]) -> tuple[dict[Player, Card], list[Player]]:
+    """
+    Generates a random player order by using list of active players.
+    Returns a tuple containing player order defined by a random card pull measure, using as keys the players and card assignment.
+    """
     card_per_player: dict[Player, Card] = {}
     hand: Stack = _get_hand(len(players))
 
@@ -35,11 +45,19 @@ def get_players_order(players: list[Player]) -> tuple[dict[Player, Card], list[P
     return card_per_player, [player_card[0] for player_card in playing_order]
 
 def _get_hand(number_of_different_cards: int) -> Stack:
+    """
+    Function generates a random set number of cards based on variable.
+    Returns object of Stack type, representing a collection of cards.
+    """
     deck = Deck()
     deck.shuffle()
     return deck.deal(number_of_different_cards)
 
 def _get_reformatted_card(card: PyDealerCard) -> tuple[str, list[str]]:
+    """
+    Function performs reformation of cards for respective visual representations.
+    Returns tuple containing card values and card suits assigned.
+    """
     card_value: str = _SHORT_VALUE[card.value] if card.value in const.VALUES[9:] else card.value
     card_suit: list[str] = _SYMBOLS[card.suit]
     return card_value, card_suit
